1f450ea2a97606b479a44daa2ba35f70f671326b
Karl Schärlund <kalle@scharlund.se>
2020-12-08 19:40:18 +0100

    2020-12-03

---
+++ 03/b.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def main():
+    trees = list(x.strip() for x in sys.stdin.readlines())
+    count_prod = 1
+    for dx, dy in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
+        count = 0
+        col = dx
+        row = dy
+        while row < len(trees):
+            line = trees[row]
+            count += (line[col] == '#')
+            row += dy
+            col = (col + dx) % len(line)
+        count_prod *= count
+    print(count_prod)
+
+if __name__ == '__main__':
+    main()
