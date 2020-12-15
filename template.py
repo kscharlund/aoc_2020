dd2c8033e75f1940839ee39b0930895c8dd14905
Karl Schärlund <kalle@maxflow.org>
2020-12-15 11:52:59 +0100

    Add memoize to template

--- template.py
+++ template.py
@@ -1,5 +1,18 @@
 import sys
 from pprint import pprint
+import math
+
+
+def memoize(func):
+    """
+    Memoization decorator for a function taking a single argument.
+    """
+    class Memodict(dict):
+        """Memoization dictionary."""
+        def __missing__(self, key):
+            ret = self[key] = func(key)
+            return ret
+    return Memodict().__getitem__
 
 
 def a():
